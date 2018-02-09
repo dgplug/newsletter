fd170d70290468400580d9a251ae680d37704d32
schubisu <robin.schubert@ghi-muenster.de>
2018-02-09 11:59:05 +0100

    added new resource: twitter psf python-news list, added twitter support for collector script

--- scripts/news_collector.py
+++ scripts/news_collector.py
@@ -4,6 +4,8 @@ import feedparser
 import dateutil.parser as parser
 import datetime
 import pprint
+import requests
+from bs4 import BeautifulSoup
 
 
 def get_rss_feeds(source, newer_than=None):
@@ -16,12 +18,40 @@ def get_rss_feeds(source, newer_than=None):
         {
             'title': entry.get('title', 'None'),
             'author': entry.get('author', 'unknown'),
-            'updated': entry.updated,
+            'date': entry.updated,
             'link': entry.link
         }
         for entry in entries
     ]
     return entry_summary
+
+
+def get_twitterlist_tweets(source, newer_than=None):
+    response = requests.get(source, headers = {"Accept-Language": "en-US"})
+    soup = BeautifulSoup(response.text, 'lxml')
+    tweets = soup.find_all('li', attrs={"data-item-type": "tweet"})
+
+    if newer_than:
+        tweets = [
+            tweet for tweet in tweets
+            if parser.parse(
+                    tweet.find('a', attrs={"class": "tweet-timestamp"}).text
+            ).date() > newer_than
+        ]
+
+    tweet_summary = [
+        {
+            'title': tweet.find('p', attrs={'class': 'tweet-text'}).text,
+            'author': tweet.find('span', attrs={'class': 'username'}).text,
+            'date': parser.parse(
+                tweet.find('a', attrs={'class': 'tweet-timestamp'}).text
+            ).date(),
+            'link': "https://twitter.com" +
+            tweet.find('a', attrs={'class': 'tweet-timestamp'}).get('href')
+        }
+        for tweet in tweets
+    ]
+    return tweet_summary
 
 
 def main():
@@ -35,9 +65,14 @@ def main():
             newer_than=two_weeks
         )
 
+    for twitter_source in resources['twitter']:
+        summaries[twitter_source['name']] = get_twitterlist_tweets(
+            twitter_source['url'],
+            newer_than=two_weeks
+        )
+
     pprint.pprint(summaries)
 
 
 if __name__ == "__main__":
     main()
-    
